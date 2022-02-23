83305ce45dde7fdf8b86ae1f7393e8530f0e8399
samuel wuitron mendoza <cwuitron@ucol.mx>
2022-02-22 23:56:50 -0600

    Archivos faltantes del proyecto

--- App.js
+++ App.js
@@ -2,41 +2,30 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View, Button} from 'react-native';
 
-const style = {
-  fontSize: 38,
-};
 
-const NuevoComponente = ({title}) => {
-  return <Text style={style}>{title}</Text>;
-};
 
 const App = () => {
   
-  const [seconds, setSeconds] = useState(0);
-  const [minutos, setMinutos] = useState(0);
-    
-
-    useEffect(() => {
-      
-      const interval = setInterval(() => {
-        setSeconds (seconds => seconds + 1);
-      }, 1000);
-       
-        if (seconds == 60) {
-          setMinutos (minutos => minutos + 1);
-          setSeconds (0);
-        };
-        return () => clearInterval(interval);
-
-  }, [seconds]);
-  
-
-  
-
+  const peticion = () =>{
+    var url = 'https://calculadora-server.herokuapp.com/states-control';
+    const body={
+      accountNumber: 20161164,
+      name: 'Cristopher Samuel Wuitron Mendoza',
+      age: 21,
+    };
+    fetch(url, {
+        method:'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(res => res.json())
+    .catch(error => console.error('Error', error))
+    .then(response => console.log('Success:', response));
+  };
   return (
     <View>
-      <NuevoComponente title="Contador:" />
-      <Text style={{fontSize: 40}}> {minutos < 10 ? '0' + minutos : minutos} : {(seconds < 10) ? '0' + seconds : seconds}</Text>
+      <Button title="Comenzar" onclick={peticion()}></Button>
       
     </View>
   );
